9e42040c0b7f9b2f4ef042e6cb94a253954f4d51
Avionics <57433930+RiSE4@users.noreply.github.com>
2025-12-30 09:03:20 +0000

    index.jspを表示できるようにする

--- webapps/sample/WEB-INF/classes/SampleServlet.java
+++ webapps/sample/WEB-INF/classes/SampleServlet.java
@@ -6,14 +6,14 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/sample")
+@WebServlet("/sample/sample")
 public class SampleServlet extends HttpServlet {
 
     @Override
